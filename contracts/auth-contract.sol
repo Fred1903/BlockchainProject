92493a9fd8a7a047cc322e8aeaa05e56f641bb80
Friedrich Röben <100136262+Fred1903@users.noreply.github.com>
2025-01-09 19:22:46 +0100

    b

--- contracts/auth-contract.sol
+++ contracts/auth-contract.sol
@@ -10,16 +10,25 @@ contract SimpleAccess {
     
     // Event émis quand quelqu'un obtient l'accès
     event AccessGranted(address indexed user);
+    //Quand on lui l'enlève
+    event AccessRevoked(address indexed user);
     
     // Fonction pour obtenir l'accès
     function getAccess() public payable {
-        require(msg.value >= ACCESS_PRICE, "Wrong amount sent");
+        //doit etre avant la ligne suivante sinon on aura dépensé déja
         require(!hasAccess[msg.sender], "You already have access");
+        require(msg.value >= ACCESS_PRICE, "Wrong amount sent");
         
-        hasAccess[msg.sender] = true;
-        emit AccessGranted(msg.sender);
+        hasAccess[msg.sender] = true;//on lui donne l'accès
+        emit AccessGranted(msg.sender);//Et on emet l'événemnt
     }
     
+    function removeAccess(address user) public {
+        require(hasAccess[user], "User does not have access");
+        hasAccess[user] = false;
+        emit AccessRevoked(user); // Émet un événement pour suivre la suppression
+    }
+
     // Fonction pour vérifier l'accès
     function checkAccess(address user) public view returns (bool) {
         return hasAccess[user];
